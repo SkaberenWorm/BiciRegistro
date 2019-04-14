208b2d0e79cf702315588958ea002474747c174c
Isma <ismael.c.26a@gmail.com>
2019-04-14 19:18:45 -0400

    Se modifican las rutas y se crean los controladores userController y roleController

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 
-        'route' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
-        'permission' =>\Caffeinated\Shinobi\Middleware\UserHasPermission::class,
+        'route' =>  \Caffeinated\Shinobi\Middleware\UserHasRole::class,
+        'permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -22,80 +22,80 @@ Route::get('/home', 'HomeController@index')->name('home');
 // ROUTES
 
 // Envolvemos todas las rutas ya que se requiere utenticación 
-Route::middelware(['auth'])->group(function(){
+Route::middleware(['auth'])->group(function(){
     
     /*
-    ->middelware('permission:roles.create');
+    ->middleware('permission:roles.create');
         permission hace referencia a /app/Http/Kernel.php 
         $routeMiddleware = [ 
         'permission' =>\Caffeinated\Shinobi\Middleware\UserHasPermission::class
         ]
     */
-
+        
     // ROLES
     Route::post('roles/store', 'RoleController@store')->name('roles.store')
-            ->middelware('permission:roles.create');
+            ->middleware('permission:roles.create');
             
     Route::get('roles', 'RoleController@index')->name('roles.index')
-            ->middelware('permission:roles.index');
+            ->middleware('permission:roles.index');
 
     Route::get('roles/create', 'RoleController@create')->name('roles.create')
-            ->middelware('permission:roles.create');
+            ->middleware('permission:roles.create');
 
     Route::put('roles/update', 'RoleController@update')->name('roles.update')
-            ->middelware('permission:roles.edit');
+            ->middleware('permission:roles.edit');
 
     Route::get('roles/{role}', 'RoleController@show')->name('roles.show')
-            ->middelware('permission:roles.show');
+            ->middleware('permission:roles.show');
 
     Route::delete('roles/{role}', 'RoleController@destroy')->name('roles.destroy')
-            ->middelware('permission:roles.destroy');
+            ->middleware('permission:roles.destroy');
 
     Route::get('roles/{role}/edit', 'RoleController@edit')->name('roles.edit')
-            ->middelware('permission:roles.edit');
+            ->middleware('permission:roles.edit');
 
     // VEHICULOS
     Route::post('vehiculos/store', 'VehiculoController@store')->name('vehiculos.store')
-            ->middelware('permission:vehiculos.create');
+            ->middleware('permission:vehiculos.create');
             
     Route::get('vehiculos', 'VehiculoController@index')->name('vehiculos.index')
-            ->middelware('permission:vehiculos.index');
+            ->middleware('permission:vehiculos.index');
 
     Route::get('vehiculos/create', 'VehiculoController@create')->name('vehiculos.create')
-            ->middelware('permission:vehiculos.create');
+            ->middleware('permission:vehiculos.create');
 
     Route::put('vehiculos/update', 'VehiculoController@update')->name('vehiculos.update')
-            ->middelware('permission:vehiculos.edit');
+            ->middleware('permission:vehiculos.edit');
 
     Route::get('vehiculos/{role}', 'VehiculoController@show')->name('vehiculos.show')
-            ->middelware('permission:vehiculos.show');
+            ->middleware('permission:vehiculos.show');
 
     Route::delete('vehiculos/{role}', 'VehiculoController@destroy')->name('vehiculos.destroy')
-            ->middelware('permission:vehiculos.destroy');
+            ->middleware('permission:vehiculos.destroy');
 
     Route::get('vehiculos/{role}/edit', 'VehiculoController@edit')->name('vehiculos.edit')
-            ->middelware('permission:vehiculos.edit');
+            ->middleware('permission:vehiculos.edit');
 
     // USUARIOS
     Route::post('users/store', 'UserController@store')->name('users.store')
-            ->middelware('permission:users.create');
+            ->middleware('permission:users.create');
             
     Route::get('users', 'UserController@index')->name('users.index')
-            ->middelware('permission:users.index');
+            ->middleware('permission:users.index');
 
     Route::get('users/create', 'UserController@create')->name('users.create')
-            ->middelware('permission:users.create');
+            ->middleware('permission:users.create');
 
     Route::put('users/update', 'UserController@update')->name('users.update')
-            ->middelware('permission:users.edit');
+            ->middleware('permission:users.edit');
 
     Route::get('users/{role}', 'UserController@show')->name('users.show')
-            ->middelware('permission:users.show');
+            ->middleware('permission:users.show');
 
     Route::delete('users/{role}', 'UserController@destroy')->name('users.destroy')
-            ->middelware('permission:users.destroy');
+            ->middleware('permission:users.destroy');
 
     Route::get('users/{role}/edit', 'UserController@edit')->name('users.edit')
-            ->middelware('permission:users.edit');
+            ->middleware('permission:users.edit');
             
 });
